30ba611f633b0219fb7dc5f8bd89e71976237c38
Hedy Wang <hedywings@gmail.com>
2017-03-29 19:10:28 +0800

    update ble_connect

--- examples/ble_connect.js
+++ examples/ble_connect.js
@@ -1,4 +1,5 @@
-var ccBnp = require('../index.js');
+var _ = require('busyman'),
+    ccBnp = require('../index.js');
 
 var cfg = {
     path: '/dev/ttyACM0',
@@ -8,48 +9,49 @@ var cfg = {
 };
 
 ccBnp.on('ready', function(result) {
-    //console.log(result);
-    
-    ccBnp.gap.deviceDiscReq(3, 1, 0, function (err, result) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(result[1]);
-            if (result[1].GapDeviceInfo0) {
-                ccBnp.gap.estLinkReq(0, 0, 0, '0x78c5e5707a06', function (err, result) {
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        ccBnp.gatt.readMultiCharValues(result[1].GapLinkEstablished.connHandle, { handle0: 0x0025, handle1: 0x0037 }, function (err, result) {
-                            if (err) console.log(err);
-                            else console.log(result[1]);
-                        });
-                    }
-                });
-            }
-        }
+    ccBnp.gap.deviceDiscReq(3, 1, 0).then(function (result) {
+        if (result.collector.GapDeviceInfo) 
+            return ccBnp.gap.estLinkReq(1, 0, 0, result.collector.GapDeviceInfo[0].addr);
+    }).then(function (result) {
+        if (result)
+            return ccBnp.gatt.readCharValue(result.collector.GapLinkEstablished[0].connHandle, 0x0003, '0x2a00');
+    }).then(function (result) {
+        if (result)
+            console.log('Device Name: ' + result.collector.AttReadRsp[0].value.name);
+    }).fail(function (err) {
+        console.log(err);
     });
 });
 
 ccBnp.on('ind', function(msg){
     switch (msg.type) {
         case 'linkEstablished':
+            console.log(' ');
+            console.log('----Event: linkEstablished----');
             console.log(msg);
             console.log(' ');
             break;
         case 'linkTerminated':
+            console.log(' ');
+            console.log('----Event: linkTerminated----');
             console.log(msg);
             console.log(' ');
             break;
         case 'linkParamUpdate':
+            console.log(' ');
+            console.log('----Event: linkParamUpdate----');
             console.log(msg);
             console.log(' ');
             break;
         case 'attNoti':
+            console.log(' ');
+            console.log('----Event: attNoti----');
             console.log(msg);
             console.log(' ');
             break;
         case 'attInd':
+            console.log(' ');
+            console.log('----Event: attInd----');
             console.log(msg);
             console.log(' ');
             break;
